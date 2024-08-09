858b1026d70f616659d6de3a17894834a6d6bbb8
Joanadayse <daysejoana3@gmail.com>
2024-08-09 07:27:57 -0300

    ajuste nas normas da criptografia

--- script.js
+++ script.js
@@ -1,88 +1,60 @@
-const substituicao = {
-  a: "m",
-  b: "n",
-  c: "b",
-  d: "v",
-  e: "c",
-  f: "x",
-  g: "z",
-  h: "a",
-  i: "s",
-  j: "d",
-  k: "f",
-  l: "g",
-  m: "h",
-  n: "j",
-  o: "k",
-  p: "l",
-  q: "p",
-  r: "o",
-  s: "i",
-  t: "u",
-  u: "y",
-  v: "t",
-  w: "r",
-  x: "e",
-  y: "w",
-  z: "q",
-  A: "M",
-  B: "N",
-  C: "B",
-  D: "V",
-  E: "C",
-  F: "X",
-  G: "Z",
-  H: "A",
-  I: "S",
-  J: "D",
-  K: "F",
-  L: "G",
-  M: "H",
-  N: "J",
-  O: "K",
-  P: "L",
-  Q: "P",
-  R: "O",
-  S: "I",
-  T: "U",
-  U: "Y",
-  V: "T",
-  W: "R",
-  X: "E",
-  Y: "W",
-  Z: "Q",
+const chaves = {
+  e: "enter",
+  i: "imes",
+  a: "ai",
+  o: "ober",
+  u: "ufat",
 };
 
-const reverseSubstituicao = Object.fromEntries(
-  Object.entries(substituicao).map(([key, value]) => [value, key])
+const chavesReversas = Object.fromEntries(
+  Object.entries(chaves).map(([key, value]) => [value, key])
 );
 
 function criptografar() {
-  const texto = document.getElementById("texto").value;
+  let texto = document.getElementById("texto").value;
   let resultado = "";
+
+  if (!/^[a-z\s]+$/.test(texto)) {
+    alert(
+      "Por favor, use apenas letras minúsculas sem acentos e sem caracteres especiais."
+    );
+    return;
+  }
 
   for (let i = 0; i < texto.length; i++) {
     const char = texto[i];
-    resultado += substituicao[char] || char;
+    resultado += chaves[char] || char; // Substitui ou mantém o caractere se não estiver no mapeamento
   }
 
   document.getElementById("resultado").innerText = resultado;
 }
 
 function descriptografar() {
-  const texto = document.getElementById("texto").value;
+  let texto = document.getElementById("texto").value;
+
+
+  if (!/^[a-z\s]+$/.test(texto)) {
+    alert(
+      "Por favor, use apenas letras minúsculas sem acentos e sem caracteres especiais."
+    );
+    return;
+  }
+
+  for (const [key, value] of Object.entries(chavesReversas)) {
+    texto = texto.split(key).join(value); 
+  }
 
   document.getElementById("resultado").innerText = texto;
 }
 
-  function copiarResultado() {
-    const resultado = document.getElementById("resultado").innerText;
-    navigator.clipboard
-      .writeText(resultado)
-      .then(() => {
-        alert("Texto copiado para a área de transferência");
-      })
-      .catch((err) => {
-        console.error("Erro ao copiar o texto: ", err);
-      });
-  }
+function copiarResultado() {
+  const resultado = document.getElementById("resultado").innerText;
+  navigator.clipboard
+    .writeText(resultado)
+    .then(() => {
+      alert("Texto copiado para a área de transferência");
+    })
+    .catch((err) => {
+      console.error("Erro ao copiar o texto: ", err);
+    });
+}
